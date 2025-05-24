6d8de14d5a4ec39e1cb5d10ff4447fb69bd695b0
David Douwsma <d.l.douwsma@student.tudelft.nl>
2025-05-24 10:36:01 +0200

    strava updates now working

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # strava-surf-sync
 I use this to change the sup activity from sup to surfing and then add some extra data from the garmin connect app about the surfing I did. I also make all activities that are not surfing public via this.
-
-Hi, this project is going to be awesome. I love computer science.
--- sync.py
+++ sync.py
@@ -2,8 +2,11 @@ import os
 import requests
 from garminconnect import Garmin
 from dotenv import load_dotenv
-from dateutil import parser
+from dateutil import parser, tz
 from fitparse import FitFile
+from datetime import timezone
+cet = tz.gettz("Europe/Amsterdam")
+import fitdecode
 
 load_dotenv()
 
@@ -17,7 +20,58 @@ STRAVA_ACCESS_TOKEN = os.getenv("STRAVA_ACCESS_TOKEN")
 
 STRAVA_API_BASE = "https://www.strava.com/api/v3"
 
+def authorize_strava_with_write_scope():
+    import webbrowser
+    from urllib.parse import urlparse, parse_qs
+
+    client_id = os.getenv("STRAVA_CLIENT_ID")
+    redirect_uri = "http://localhost"
+    scope = "activity:write"
+
+    auth_url = (
+        f"https://www.strava.com/oauth/authorize?"
+        f"client_id={client_id}&response_type=code"
+        f"&redirect_uri={redirect_uri}"
+        f"&approval_prompt=force"
+        f"&scope={scope}"
+    )
+
+    print("Opening browser for Strava OAuth...")
+    webbrowser.open(auth_url)
+    redirected_url = input("Paste the full redirected URL here:\n")
+    
+    # Extract code from redirected URL
+    code = parse_qs(urlparse(redirected_url).query).get("code", [None])[0]
+    if not code:
+        raise Exception("Authorization code not found.")
+    return code
+
+def exchange_strava_code_for_tokens(code):
+    client_id = os.getenv("STRAVA_CLIENT_ID")
+    client_secret = os.getenv("STRAVA_CLIENT_SECRET")
+
+    response = requests.post("https://www.strava.com/oauth/token", data={
+        "client_id": client_id,
+        "client_secret": client_secret,
+        "code": code,
+        "grant_type": "authorization_code"
+    })
+    response.raise_for_status()
+    tokens = response.json()
+
+    # Optional: Save to .env
+    with open(".env", "a") as f:
+        f.write(f"\nSTRAVA_ACCESS_TOKEN={tokens['access_token']}")
+        f.write(f"\nSTRAVA_REFRESH_TOKEN={tokens['refresh_token']}")
+        f.write(f"\nSTRAVA_EXPIRES_AT={tokens['expires_at']}")
+    
+    return tokens
+
 def refresh_strava_access_token():
+    STRAVA_CLIENT_ID = os.getenv("STRAVA_CLIENT_ID")
+    STRAVA_CLIENT_SECRET = os.getenv("STRAVA_CLIENT_SECRET")
+    STRAVA_REFRESH_TOKEN = os.getenv("STRAVA_REFRESH_TOKEN")
+
     url = "https://www.strava.com/oauth/token"
     payload = {
         "client_id": STRAVA_CLIENT_ID,
@@ -25,12 +79,24 @@ def refresh_strava_access_token():
         "refresh_token": STRAVA_REFRESH_TOKEN,
         "grant_type": "refresh_token"
     }
+
     response = requests.post(url, data=payload)
     response.raise_for_status()
     tokens = response.json()
-    access_token = tokens["access_token"]
 
-    return access_token
+    if tokens["refresh_token"] != STRAVA_REFRESH_TOKEN:
+        with open(".env", "r") as f:
+            lines = f.readlines()
+
+        with open(".env", "w") as f:
+            for line in lines:
+                if line.startswith("STRAVA_REFRESH_TOKEN="):
+                    f.write(f"STRAVA_REFRESH_TOKEN={tokens['refresh_token']}\n")
+                else:
+                    f.write(line)
+
+    return tokens["access_token"], tokens["refresh_token"], tokens["expires_at"]
+
 
 def get_strava_activities(access_token, per_page=30):
     url = f"{STRAVA_API_BASE}/athlete/activities"
@@ -59,12 +125,15 @@ def update_strava_activity(access_token, activity_id, data):
     response.raise_for_status()
     return response.json()
 
+from datetime import timezone
+
 def find_matching_garmin_activity(strava_activity, garmin_activities, time_tolerance=120, distance_tolerance=100):
-    strava_time = parser.parse(strava_activity["start_date_local"])
+    strava_time = parser.parse(strava_activity["start_date_local"]).replace(tzinfo=cet)
     strava_distance = strava_activity.get("distance", 0)
 
+
     for garmin_activity in garmin_activities:
-        garmin_time = parser.parse(garmin_activity["startTimeLocal"])
+        garmin_time = parser.parse(garmin_activity["startTimeLocal"]).replace(tzinfo=cet)
         garmin_distance = garmin_activity.get("distance", 0)
 
         time_diff = abs((strava_time - garmin_time).total_seconds())
@@ -78,29 +147,48 @@ def find_matching_garmin_activity(strava_activity, garmin_activities, time_toler
 
 def main():
     garmin_client = authenticate_garmin()
-    strava_token = refresh_strava_access_token()
+    strava_token, new_refresh_token, expires_at = refresh_strava_access_token()
 
     garmin_activities = garmin_client.get_activities(0, 20)
     strava_activities = get_strava_activities(strava_token)
 
     for strava_activity in strava_activities:
-        if "suppen" in strava_activity["name"] and strava_activity["type"] == "Stand Up Paddling":
-            new_name = strava_activity["name"].replace("suppen", "Surfen")
-            print(f"Updating activity {strava_activity['id']} to: {new_name}")
-            update_strava_activity(strava_token, strava_activity["id"], {"name": new_name})
-
-            new_type = "Surfing"
-            print(f"Updating sport type {strava_activity["type"]} to: {new_type}")
-            update_strava_activity(strava_token, strava_activity["id"], {"type": new_type})
-
-            matching_garmin_activity = find_matching_garmin_activity(strava_activity, garmin_activities)
-            if matching_garmin_activity:
-                print(f"Matched Garmin activity ID: {matching_garmin_activity['activityId']}")
-            else:
-                print("No matching Garmin activity found...")
-        
+        if "suppen" in strava_activity["name"] and strava_activity["type"] == "StandUpPaddling":
             
+            new_name = strava_activity["name"].replace("suppen", "Surfen")
+            new_type = "Surfing"
+            print(f"Updating activity {strava_activity['id']} to: {new_name}")
+            update_strava_activity(strava_token, strava_activity["id"], {"name": new_name, "type": new_type})
+            
+            # print(f"Updating sport type {strava_activity["type"]} to: {new_type}")
+            # update_strava_activity(strava_token, strava_activity["id"], {"type": new_type})
+
+            # matching_garmin_activity = find_matching_garmin_activity(strava_activity, garmin_activities)
+            # if matching_garmin_activity:
+            #     print(f"Matched Garmin activity ID: {matching_garmin_activity['activityId']}")
+            #     activity_id = matching_garmin_activity["activityId"]
+                # fit_data = garmin_client.download_activity(activity_id, dl_fmt=Garmin.ActivityDownloadFormat.ORIGINAL)
+                # with open(f"{activity_id}.fit", "wb") as f:
+                #     f.write(fit_data)
+                
+                # fitfile = FitFile(f"{activity_id}.fit")
+
+                # for record in fitfile.get_messages("record"):
+                #     for field in record:
+                #         print(f"{field.name}: {field.value}")
+
+                # with fitdecode.FitReader(f'{activity_id}.fit') as fit:
+                #     for frame in fit:
+                #         if frame.frame_type == fitdecode.FIT_FRAME_DATA:
+                #             print(frame.name)
+                #             for field in frame.fields:
+                #                 print(f" * {field.name}: {field.value}")
+            # else:
+            #     print("No matching Garmin activity found...")
+        
 
 
 if __name__ == "__main__":
+    code = authorize_strava_with_write_scope()
+    tokens = exchange_strava_code_for_tokens(code)
     main()
