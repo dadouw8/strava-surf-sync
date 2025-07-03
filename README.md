6632b145db2b8261c5f932fd394903b508d027f9
David Douwsma <d.l.douwsma@student.tudelft.nl>
2025-07-03 09:42:48 +0200

    Updates activities and adds description for the data from garmin connect about the actual surfing. Max speed, max wave length, total wave time, etc.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # strava-surf-sync
 I use this to change the sup activity from sup to surfing and then add some extra data from the garmin connect app about the surfing I did. I also make all activities that are not surfing public via this.
+
+
+I am currently running into an issue. 
+The github actions IP is not authorized to login into garmin connect. So, if I want to do things with garmin connect, I will have to make some changes. There are a few possible solutions.
+I could use a VPN to make sure I have a trusted IP, but this can get kinda complex I think.
+So, I think I am going to use a raspberry pi to run this script every 15 minutes. This will be a lot easier to implement.
--- sync.py
+++ sync.py
@@ -4,7 +4,8 @@ import requests
 from io import BytesIO
 from dotenv import load_dotenv
 from dateutil import parser, tz
-import datetime
+from datetime import datetime, timedelta
+from dateutil import parser
 from garminconnect import (
     Garmin,
     GarminConnectConnectionError,
@@ -67,7 +68,7 @@ def get_fields(fit_files):
         fields = {}
         for message in fit_file.get_messages():
             if(message.get_value("local_timestamp") is not None):
-                fields["time_created"] = message.get_value("local_timestamp").astimezone(cet).isoformat()
+                fields["time_created"] = message.get_value("local_timestamp").astimezone(cet)
                 break
         for message in fit_file.get_messages():
             if (message.get_value("wavenum") is not None):
@@ -80,7 +81,6 @@ def get_fields(fit_files):
                 fields["max_wave_distance"] = message.get_value("wavedist")
                 fields["max_wave_speed"] = message.get_value("wavespd")
                 break
-        print(fields)
         result.append(fields)
     return result
                 
@@ -102,7 +102,7 @@ def refresh_strava_access_token():
     return tokens["access_token"], tokens["refresh_token"], tokens["expires_at"]
 
 
-def get_strava_activities(access_token, per_page=30):
+def get_strava_activities(access_token, per_page=10):
     url = f"{STRAVA_API_BASE}/athlete/activities"
     headers = {"Authorization": f"Bearer {access_token}"}
     params = {"per_page": per_page}
@@ -121,21 +121,34 @@ def main():
     strava_token, new_refresh_token, expires_at = refresh_strava_access_token()
     strava_activities = get_strava_activities(strava_token)
     garmin_activities = get_surfing_activities_garmin()
-
+    
 
 
     for strava_activity in strava_activities:
         if "suppen" in strava_activity["name"] and strava_activity["type"] == "StandUpPaddling":
-
-            for garmin_activity in garmin_activities:
-                print(garmin_activity["time_created"])
-            #     if(strava_activity["start_date"] == garmin_activity["time_created"].astimezone(cet).isoformat()):
-            #         print(f"Found matching Garmin activity for Strava activity {strava_activity['id']}: {garmin_activity}")
             
-            # new_name = strava_activity["name"].replace("suppen", "Surfen")
-            # new_type = "Surfing"
-            # print(f"Updating activity {strava_activity['id']} to: {new_name}")
-            # update_strava_activity(strava_token, strava_activity["id"], {"name": new_name, "type": new_type})
+            
+            for garmin_activity in garmin_activities:
+                tolerance = timedelta(seconds=20)
+                strava_date = parser.isoparse(strava_activity["start_date"])
+                strava_time = strava_date.astimezone(cet)
+                if abs(garmin_activity["time_created"] - strava_time) <= tolerance:
+                    print(f"Found matching Garmin activity for Strava activity {strava_activity['id']}: {garmin_activity}")
+                    description = (
+                        f"Number Of Total Waves: {garmin_activity['wavenum']}\n"
+                        f"Number Of Lefts: {garmin_activity['numlefts']}\n"
+                        f"Number Of Rights: {garmin_activity['numrights']}\n"
+                        f"Total Wave Time: {garmin_activity['total_wave_time']} seconds\n"
+                        f"Total Wave Distance: {garmin_activity['total_wave_distance']} meters\n"
+                        f"Max Wave Time: {garmin_activity['max_wave_time']} seconds\n"
+                        f"Max Wave Distance: {garmin_activity['max_wave_distance']} meters\n"
+                        f"Max Speed: {garmin_activity['max_wave_speed']} km/h"
+                    )
+            
+            new_name = strava_activity["name"].replace("suppen", "Surfen")
+            new_type = "Surfing"
+            print(f"Updating activity {strava_activity['id']} to: {new_name}")
+            update_strava_activity(strava_token, strava_activity["id"], {"name": new_name, "type": new_type, "description": description})
 
 
 if __name__ == "__main__":
